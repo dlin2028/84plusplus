3da26b82c240f2a0e915f488d3a3884f4052da50
David Lin <dlin2028@gmail.com>
2020-09-01 20:17:50 -0700

    added while staement parsing

---
+++ Parser/WhileStatement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokenizer;
+
+namespace Parser
+{
+    class WhileStatement
+    {
+        public Token WhileToken;
+        public Token OpenParenToken;
+        public ExpressionNode ExpressionNode;
+        public Token CloseParenToken;
+        public SyntaxNode Body;
+
+        public WhileStatement(Stack<Token> tokens)
+        {
+            WhileToken = tokens.Pop();
+            OpenParenToken = tokens.Pop();
+            ExpressionNode = new ExpressionNode(tokens);
+            CloseParenToken = tokens.Pop();
+            Body = new BodyNode(tokens);
+        }
+    }
+}
